5338fa94a9bb31dec9f56f0496a72eaa441dc3b4
kiyani <bamba.kiyani@gmail.com>
2020-02-10 19:08:43 -0500

    completed exercice 11

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,6 +2,12 @@
 
 # YOUR CODE HERE
 
+def is_even(num):
+      if num % 2 == 0:
+              return True
+      else:
+              return False
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -10,3 +16,9 @@ num = int(num)
 
 # YOUR CODE HERE
 
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
